b6f53117f3eed199e42b0363633ab9715d91e084
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2014-11-16 14:22:49 +0000

    The server class is no longer the main class. Created Chess-e main class.

---
+++ src/Chesse/Main.java
@@ -0,0 +1,10 @@
+package Chesse;
+
+/**
+ * Created by ruijorgeclarateixeira on 16/11/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/TurboNGServer/TurboNGServer.java
+++ src/TurboNGServer/TurboNGServer.java
@@ -13,26 +13,39 @@ import java.util.concurrent.Executors;
  * It starts a ConnectionHandler that will receive the incoming connections
  */
 public class TurboNGServer {
+    SSLServerSocket serverSocket;
+    int port;
+    int numberOfThreads;
+    boolean sslConnection;
 
-    public static void main(String[] args) {
-        SSLServerSocket serverSocket;
+    private TurboNGServer(int port, int numberOfThreads, boolean sslConnection) {
+        this.port = port;
+        this.numberOfThreads = numberOfThreads;
+        this.sslConnection = sslConnection;
+    }
+
+    public static TurboNGServer createInstance(int port, int numberOfThreads, boolean withSSLConnection) {
+        return new TurboNGServer(port, numberOfThreads, withSSLConnection);
+    }
+
+    public void start() {
         System.out.println("Starting game server.");
         try {
             System.out.println("Creating listening socket.");
-            serverSocket = TurboNGServerSocketFactory.createNGServerSocket(ServerVariables.LISTENING_PORT, true);
+            serverSocket = TurboNGServerSocketFactory.createNGServerSocket(port, sslConnection);
 
             if(serverSocket != null)
-                System.out.println("Listening on port " + ServerVariables.LISTENING_PORT);
+                System.out.println("Listening on port " + port);
             else
                 throw new RuntimeException("Could not create the listening socket!");
 
-            System.out.println("Starting Connection Handler with Thread Pool of size " + ServerVariables.NUMBER_OF_THREADS);
-            ConnectionHandler.start(Executors.newFixedThreadPool(ServerVariables.NUMBER_OF_THREADS), serverSocket);
+            System.out.println("Starting Connection Handler with Thread Pool of size " + numberOfThreads);
+            ConnectionHandler.start(Executors.newFixedThreadPool(numberOfThreads), serverSocket);
 
             System.out.println("Server is running.");
         } catch (RuntimeException e) {
             ServerVariables.errorLogger.severe("Uncaught exception! Server will stop!"
-                                                + "\nException Message" +  e.getMessage());
+                    + "\nException Message" +  e.getMessage());
             e.printStackTrace();
         }
     }
