9ff9fadda46378e01dd572dae872b4c2e7a28637
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2015-02-10 17:23:39 +0000

    Improved messaging model.

--- src/TurboNGServer/Modules/MessagingModule.java
+++ src/TurboNGServer/Modules/MessagingModule.java
@@ -12,10 +12,11 @@ public abstract class MessagingModule extends InviteByUsernameModule {
 
     /**
      * Chat message received.
+     * @param public_message True if the message is public.
      * @param source Username of the sender.
      * @param message Message received.
      */
-    public abstract void chatMessage(String source, String message);
+    public abstract void chatMessage(Boolean public_message, String source, String message);
 
     @Override
     public boolean executeAction(Action action) {
@@ -29,8 +30,7 @@ public abstract class MessagingModule extends InviteByUsernameModule {
             switch (action.getValueOf("action")) {
                 case "private":
                     if(action.getValueOf("target") != null && action.getValueOf("message") != null) {
-                        ((MessagingModule) PlayersManager.getPlayer(action.getValueOf("target"))).chatMessage(this.username,
-                                                                                            action.getValueOf("message"));
+                        sendMessage(action.getValueOf("target"), action.getValueOf("message"));
                     }
                     break;
                 case "public":
@@ -45,13 +45,22 @@ public abstract class MessagingModule extends InviteByUsernameModule {
     }
 
     /**
+     * Send private message to a single target.
+     * @param target Username of the target of the message.
+     * @param content Content of the message.
+     */
+    public void sendMessage(String target, String content) {
+        ((MessagingModule) PlayersManager.getPlayer(target)).chatMessage(false, this.username, content);
+    }
+
+    /**
      * Send message to all the players except this one
      * @param messageContent Content of the message to be send.
      */
     public void sendMessageToAll(String messageContent) {
         for(Player player : PlayersManager.getAllPlayers()) {
             if(!player.username.equals(this.username))
-                ((MessagingModule) player).chatMessage(this.username, messageContent);
+                ((MessagingModule) player).chatMessage(true, this.username, messageContent);
         }
     }
 
