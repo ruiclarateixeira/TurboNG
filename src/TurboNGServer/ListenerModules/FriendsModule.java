06caa4f35435f7cd90717735c15cd594e7cf5a40
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2015-03-08 11:13:20 +0000

    Added Friends Listener Modules with add and remove friend.

---
+++ src/TurboNGServer/ListenerModules/FriendsModule.java
@@ -0,0 +1,120 @@
+package TurboNGServer.ListenerModules;
+
+import TurboNGServer.Interface.Action;
+import TurboNGServer.Player.Player;
+import TurboNGServer.StandaloneModules.Database;
+
+/**
+ * Created by ruijorgeclarateixeira on 08/03/15.
+ * Friends listener modules. Allows user to keep a list of special users. (Friends)
+ * Requires a database to store friends relationship.
+ */
+public abstract class FriendsModule extends Player {
+    /**
+     * Regex username is matched against on registering.
+     */
+    public static String usernameRegex = "^[a-z0-9_-]{3,16}$";
+
+    /**
+     * Received action to execute.
+     *
+     * @param action Action o execute.
+     * @return True if this method can execute the action. False otherwise.
+     */
+    @Override
+    public boolean executeAction(Action action) {
+        if(super.executeAction(action))
+            return true;
+        else if (action.getValueOf("type") == null
+                || action.getValueOf("action") == null
+                || !action.getValueOf("type").equals("friends")
+                || this.username == null)
+            return false;
+
+        switch (action.getValueOf("action")) {
+            case "add_friend":
+                if (action.getValueOf("username") != null && action.getValueOf("username").matches(usernameRegex)) {
+                    addFriend(action.getValueOf("username"));
+                } else {
+                    sendToClient(new Action("{type:friends,action:friend_not_added,message:'Invalid friend username'}"));
+                }
+                break;
+            case "remove_friend":
+                if (action.getValueOf("username") != null && action.getValueOf("username").matches(usernameRegex)) {
+                    removeFriend(action.getValueOf("username"));
+                } else {
+                    sendToClient(new Action("{type:friends,action:friend_not_removed,message:'Invalid friend username'}"));
+                }
+                break;
+            default:
+                sendToClient(new Action("{type:friends,action:not_recognized}"));
+        }
+
+        return true;
+    }
+
+    /**
+     * Add friendship to database if it doesn't exist.
+     * @param friendUsername Username of required friend.
+     */
+    public void addFriend(String friendUsername) {
+        int noOfRows = Database.RowCountFromWhere("FRIENDS", "PLAYER_USERNAME = '"
+                                                + this.username
+                                                + "' AND FRIEND_USERNAME = '"
+                                                + friendUsername + "'");
+
+        if (noOfRows == 0) {
+            String sql_statement = "INSERT INTO FRIENDS (PLAYER_USERNAME,FRIEND_USERNAME)" +
+                    " VALUES ('" + this.username + "','" + friendUsername + "');";
+            Database.ExecuteStatement(sql_statement);
+            friendAdded(friendUsername);
+        }
+    }
+
+    /**
+     * Called when a new friend is successfully added.
+     * @param friendUsername Username of friend added
+     */
+    public void friendAdded(String friendUsername) {
+        sendToClient(new Action("{type:friends,action:friend_added,username:" + friendUsername + "}"));
+    }
+
+    /**
+     * Remove friendship from database.
+     * @param friendUsername Username of friend to remove
+     */
+    public void removeFriend(String friendUsername) {
+        int noOfRows = Database.RowCountFromWhere("FRIENDS", "PLAYER_USERNAME = '"
+                + this.username
+                + "' AND FRIEND_USERNAME = '"
+                + friendUsername + "'");
+
+        System.out.println(noOfRows);
+        if (noOfRows > 0) {
+            String sql_statement = "DELETE FROM FRIENDS WHERE PLAYER_USERNAME = '"
+                    + this.username + "' AND FRIEND_USERNAME = '" + friendUsername + "';";
+            System.out.println(sql_statement);
+            Database.ExecuteStatement(sql_statement);
+            friendRemoved(friendUsername);
+        }
+    }
+
+    /**
+     * Called when a friend is successfully removed
+     * @param friendUsername Username of player successfully removed.
+     */
+    public void friendRemoved(String friendUsername) {
+        sendToClient(new Action("{type:friends,action:friend_removed,username:" + friendUsername + "}"));
+    }
+
+    /**
+     * Creates a friends table to be used by this class. Uses sqlite syntax query.
+     */
+    public static void CreateFriendsTable() {
+        String sql_statement = "CREATE TABLE FRIENDS " +
+                                "(ROWID             INTEGER     PRIMARY KEY     AUTOINCREMENT," +
+                                " PLAYER_USERNAME   TEXT        NOT NULL, " +
+                                " FRIEND_USERNAME   TEXT        NOT NULL)";
+        Database.ExecuteStatement(sql_statement);
+    }
+}
