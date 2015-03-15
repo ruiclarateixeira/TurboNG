963ca7adc4da874c0db1235b98aa378f54b615f4
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2015-03-15 20:34:39 +0000

    Fixed bugs in passwordless login.

--- src/TurboNGServer/ListenerModules/PasswordlessLoginModule.java
+++ src/TurboNGServer/ListenerModules/PasswordlessLoginModule.java
@@ -15,32 +15,54 @@ public abstract class PasswordlessLoginModule extends Player {
     public boolean executeAction(Action action) {
         if(super.executeAction(action))
             return true;
-        else if (action.getValueOf("type") == null || action.getValueOf("action") == null || !action.getValueOf("type").equals("login"))
+        else if (action == null
+                || action.getValueOf("type") == null
+                || action.getValueOf("action") == null
+                || !action.getValueOf("type").equals("login"))
             return false;
 
         if (action.getValueOf("action").equals("login")) {
-            if (action.getValueOf("username") != null) {
-                username = action.getValueOf("username");
-                try {
-                    addToOnlinePlayers();
-                    sendToClient(new Action("{type:login, action:successful, username:" + this.username + "}"));
-                    for (Player player : PlayersManager.getAllPlayers()) {
-                        if (!player.username.equals(username))
-                            player.sendToClient(new Action("{type:lobby, action:new_online, username:" + this.username + "}"));
-                    }
-                } catch (NullPointerException e) {
-                    e.printStackTrace();
-                } catch (KeyAlreadyExistsException e) {
-                    sendToClient(new Action("{type:login, action:username_exists}"));
-                }
-            }
+            login(action.getValueOf("username"));
         }
         else if (action.getValueOf("action").equals("logout")) {
-            sendToClient(new Action("{type:login, action:logout}"));
-            PlayersManager.removePlayer(this.username);
-            this.disconnect();
+            logout();
         }
 
         return true;
+    }
+
+    public void login(String username) {
+        if (username == null || !username.matches(usernameRegex)) {
+            sendToClient(new Action("{type:login, action:username_invalid}"));
+            return;
+        }
+        else if (this.username != null) {
+            sendToClient(new Action("{type:login, action:already_logged_in}}"));
+            return;
+        }
+
+        try {
+            this.username = username;
+            addToOnlinePlayers();
+            sendToClient(new Action("{type:login, action:successful, username:" + this.username + "}"));
+            for (Player player : PlayersManager.getAllPlayers()) {
+                if (!player.username.equals(username))
+                    player.sendToClient(new Action("{type:lobby, action:new_online, username:" + this.username + "}"));
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (KeyAlreadyExistsException e) {
+            sendToClient(new Action("{type:login, action:username_exists}"));
+        }
+    }
+
+    public void logout() {
+        if (this.username == null) {
+            sendToClient(new Action("{type:login, action:not_logged_in}}"));
+            return;
+        }
+        sendToClient(new Action("{type:login, action:logout}"));
+        PlayersManager.removePlayer(this.username);
+        this.disconnect();
     }
 }
